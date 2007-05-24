9ae5a1cab72884132f3bd98b3359bbda3a2f975c
elicriffield <elicriffield@c5c97c97-aa24-0410-9156-d8a29eb37f88>
2007-05-24 20:03:03 +0000

    More metadata
    git-svn-id: https://sipie.svn.sourceforge.net/svnroot/sipie@7 c5c97c97-aa24-0410-9156-d8a29eb37f88

--- setup.py
+++ setup.py
@@ -1,17 +1,47 @@
 #!/usr/bin/env python
+"""Pronounced SY PIE, like "sirius python", sipie is a library and a online player for Sirius online Internet streaming. It requires a login to Sirius's streaming, and both guest and subscriber logins are supported. It provides the a back end and a usable cli and gui example"""
+
+
+classifiers = """\
+Development Status :: 4 - Beta
+Environment :: Console
+Environment :: X11 Applications :: GTK
+Intended Audience :: End Users/Desktop
+Intended Audience :: Developers
+License :: OSI Approved :: GNU General Public License (GPL)
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Multimedia :: Sound/Audio :: Players
+Topic :: Software Development :: Libraries :: Python Modules
+"""
 
 from distutils.core import setup
+import sys
+
+if sys.version_info < (2, 3):
+    _setup = setup
+    def setup(**kwargs):
+        if kwargs.has_key("classifiers"):
+            del kwargs["classifiers"]
+        _setup(**kwargs)
+
+
+doclines = __doc__.split("\n")
 
 setup(name='Sipie',
       version='0.1179843539',
-      description='Player for Sirius Online Streaming',
+      description='Library and Player for Sirius Online Streaming',
       author='Eli Criffield',
       author_email='python@zendo.net',
       url='http://eli.criffield.net/sipie/',
-      scripts=[ 'sipie.py' ],
+      license='http://www.gnu.org/licenses/gpl.txt',
+      platform=['any'],
+      description = doclines[0],
+      classifiers = filter(None, classifiers.split("\n")),
       packages=['Sipie'],
+      scripts=[ 'sipie.py' ],
       data_files=[
                   ( '/usr/share/sipie/', ['sipie.png'] ),
                   ( '/usr/share/sipie/', ['Sipie/glade/gtkSipie.glade'] )
-                 ]
+                 ],
      )
