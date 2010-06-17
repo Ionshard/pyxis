dcc477b4354ce3798eb556038cb356979b335a7f
Corey Ling <kasuko@gmail.com>
2010-06-17 19:12:30 -0400

    Code move

--- Sipie/Interface.py
+++ Sipie/Interface.py
@@ -2,23 +2,57 @@
 # -*- coding: utf-8 -*-
 # Eli Criffield < pyeli AT zendo DOT net >
 # Licensed under GPLv2 See: http://www.gnu.org/licenses/gpl.txt
+from Config import Config
+from Player import Player
+import StreamHandler
+import sys 
+import os 
+import time
+import atexit
+display = True
+try:
+    os.environ['DISPLAY']
+except:
+    display = False
+if display:
+    import pynotify
+
+class Completer:
+    def __init__(self, words):
+        self.words = words
+        self.prefix = None
+    def complete(self, prefix, index):
+        if prefix != self.prefix:
+            self.matching_words = [
+                w for w in self.words if w.startswith(prefix)
+                ]
+            self.prefix = prefix
+        try:
+            return self.matching_words[index]
+        except IndexError:
+            return None
+
+def ask4Stream():
+    try:
+        stream = raw_input("\nsipie: ")
+        stream = stream.strip()
+    except (EOFError, KeyboardInterrupt):
+        sys.exit(0)
+    #print 'ask4Stream "%s"'%stream #DEBUG
+    return stream
+
+def onExit():
+   #global histfile, readline, sipie
+   try:
+       readline.write_history_file(histfile)
+   except:
+       pass
+   try:
+       sipie.close()
+   except:
+       pass
 
 def repl():
-    from Config import Config
-    from Player import Player
-    import StreamHandler
-    import sys 
-    import os 
-    import time
-    import atexit
-    display = True
-    try:
-        os.environ['DISPLAY']
-    except:
-        display = False
-    if display:
-        import pynotify
-
     global histfile, readline, sipie
 
     haveReadline = True
@@ -27,41 +61,6 @@ def repl():
     except ImportError:
         haveReadline = False
         
-    class Completer:
-        def __init__(self, words):
-            self.words = words
-            self.prefix = None
-        def complete(self, prefix, index):
-            if prefix != self.prefix:
-                self.matching_words = [
-                    w for w in self.words if w.startswith(prefix)
-                    ]
-                self.prefix = prefix
-            try:
-                return self.matching_words[index]
-            except IndexError:
-                return None
-
-    def ask4Stream():
-        try:
-            stream = raw_input("\nsipie: ")
-            stream = stream.strip()
-        except (EOFError, KeyboardInterrupt):
-            sys.exit(0)
-        #print 'ask4Stream "%s"'%stream #DEBUG
-        return stream
-
-    def onExit():
-       #global histfile, readline, sipie
-       try:
-           readline.write_history_file(histfile)
-       except:
-           pass
-       try:
-           sipie.close()
-       except:
-           pass
-
     try:
         os.remove('debug.log')
     except:
