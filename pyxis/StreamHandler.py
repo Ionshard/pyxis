08503faafc9ada2756b8b8d4f62115ceb44d945b
John Rabotnik <john.rabotnik@googlemail.com>
2010-06-24 18:36:01 +0100

    [StreamHandler] remove unused time import

--- pyxis/StreamHandler.py
+++ pyxis/StreamHandler.py
@@ -16,7 +16,7 @@
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import sys, os, time, subprocess
+import sys, os, subprocess
 import fcntl
 from Config import Config, toBool
 
