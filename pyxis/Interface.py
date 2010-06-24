3cfbdcff8b6a07f68e8f6a5c6eb265501f82ecdc
John Rabotnik <john.rabotnik@googlemail.com>
2010-06-24 19:03:41 +0100

    [Interface] fix notify icon location regression
    The notify icon path got messed up when pyxis.sh was moved to the bin
    directory (bin/pyxis/data/dog_white_outline.svg). Changed it
    to ../pyxis/data/dog_white_outline.svg"

--- pyxis/Interface.py
+++ pyxis/Interface.py
@@ -120,8 +120,8 @@ class Interface(object):
                 if not self.options.quiet:
                     print playing['logfmt']
                 if display and pynotify.init("Pyxis"):
-                    n = pynotify.Notification("Sirius", playing['logfmt'][15:],
-                            sys.path[0] + "/Pyxis/data/dog_white_outline.svg")
+                    icon = os.path.normpath(os.path.join(sys.path[0], '..')) + "/pyxis/data/dog_white_outline.svg"
+                    n = pynotify.Notification("Sirius", playing['logfmt'][15:], icon)
                     n.show()
             try:
                 time.sleep(30)
