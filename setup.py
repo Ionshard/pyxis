62bae97ff3ab2b63c4024fdfb3a98a4330c00ee0
Corey Ling <kasuko@gmail.com>
2010-12-30 13:07:45 -0500

    new version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Pyxis',
-      version='0.1',
+      version='0.2',
       description='Command Line XM and SIRIUS Radio Player',
       author='Corey Ling',
       author_email='kasuko@gmail.com',
