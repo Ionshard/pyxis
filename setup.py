add3bea3d130f759ea88ef4ceb38900808d3de75
Corey Ling <kasuko@gmail.com>
2011-02-07 11:46:51 -0500

    Update setup.py and actuall use it to upload to PyPi

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
-setup(name='Pyxis',
-      version='0.2',
+setup(name='pyxis',
+      version='0.2.1',
       description='Command Line XM and SIRIUS Radio Player',
       author='Corey Ling',
-      author_email='kasuko@gmail.com',
-      url='http://kasuko.github.com/pyxis',
+      author_email='kasuko@ionshard.com',
+      url='http://www.ionshard.com/pyxis',
       packages=['pyxis'],
       package_data={'pyxis' : ['data/*']},
       scripts=['bin/pyxis'],
